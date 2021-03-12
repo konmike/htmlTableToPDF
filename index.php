40b8a7a77af9ab2fa4327206a4dbac8a176e68c3
konmike <michalkonecny10@gmail.com>
2021-03-12 09:34:04 +0100

    Change html structure to html5

--- index.php
+++ index.php
@@ -4,102 +4,102 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HTML to PDF parser</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="jquery-ui.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css">
+    <link rel="stylesheet" href="css\old_style.css">
+    <link rel="stylesheet" href="src\scss\vendor\jquery-ui.css">
+    <link rel="stylesheet" href="node_modules\@fortawesome\fontawesome-free\css\all.min.css">
 </head>
 <body>
+    <main role="main" class="main">
+        <aside class="help help__left">
 
-<div class="wrapper wrapper--full-content">
-    <form action="makepdf.php" method="post" class="form" enctype="multipart/form-data">
-<!--         
-        <div class="wrapper wrapper--file">
-            <label class="label label--file" for="myfile">
-            <input type="file" name="myfile" id="myfile">
-            <span class="file--custom"></span>
-            </label>
-        </div> -->
+        </aside>
 
-        <div class="wrapper wrapper--file">
-            <input class="file-input" id="js-file-input" name="myfile" type="file">
-            <div class="file-content">
-            <div class="file-infos">
-                <p class="file-icon"><i class="fas fa-file-upload fa-2x"></i>
-                <span class="icon-shadow"></span>
-                <span>CLICK <span class="has-drag"> OR DRAG AND DROP</span></span>
-                </p>
-            </div>
-                <p class="file-name" id="js-file-name">Žádný soubor nebyl vybrán...</p>
-            </div>
-        </div>
-            
-        <div class="wrapper wrapper--number" id="counter">
-            <span>Počet studentů</span>
-            <div class="counter-minus btn btn-primary">-</div>
-                <input type="text" value="1" name="pocetStudentu">
-            <div class="counter-plus btn btn-primary">+</div>
-        </div>
-                    
-        <div class="wrapper wrapper--number" id="counter2">
-            <span>Počet úloh</span>
-            <div class="counter-minus btn btn-primary">-</div>
-                <input type="text" value="3" name="pocetUloh">
-            <div class="counter-plus btn btn-primary">+</div>
-        </div>
-            
-        <div class="wrapper wrapper--checkbox">
-            <span>Je součástí prohlášení?</span>
-            <input type="checkbox" id="isThereProhlaseni" name="isThereProhlaseni">
-            <label class="label label--prohlaseni" for="isThereProhlaseni">
-                <div class="check"></div>
-            </label>
-        </div>
-        <div class="wrapper wrapper--checkbox">
-            <span>Chci tisknout prohlášení?</span>
-            <input type="checkbox" id="printProhlaseni" name="printProhlaseni">
-            <label class="label label--prohlaseni" for="printProhlaseni">
-                <div class="check"></div>
-            </label>
-        </div>
-        <div class="wrapper wrapper--checkbox">
-            <span>Chci tisknout i zadání úloh?</span>
-            <input type="checkbox" id="zadani" name="zadani" checked>
-            <label class="label label--zadani" for="zadani">
-                <div class="check"></div>
-            </label>
-        </div>
-
-        <div class="wrapper wrapper--checkbox">
-            <span>Zahrnout všechny studenty</span>
-            <input type="checkbox" id="zahrnout-vse" name="zahrnout-vse" checked>
-            <label class="label label--zahrnout-vse" for="zahrnout-vse">
-                <div class="check"></div>
-            </label>
-        </div>
-
-        <div id="students-name" class="wrapper wrapper--students-name">
-        <span>Jména studentů:</span>
-            <input type="text" name="jmenaStudentu" autocomplete="off" placeholder="Jmena studentu oddelena carkou">
-        </div>
-
-
-        <div class="wrapper wrapper--number" id="counter3">
-            <span>Počet souborů:</span>
-            <div id="counter3-minus" class="counter-minus btn btn-primary">-</div>
-                <input type="text" value="0" name="pocetSouboru">
-            <div id="counter3-plus" class="counter-plus btn btn-primary">+</div>
-        </div>
-
-        <div id="generated-input" class="wrapper wrapper--generated-input">
-        </div>
-            
+        <form action="makepdf.php" method="post" class="form" enctype="multipart/form-data">
         
-        <input type="submit" value="Stáhnout">
+            <div class="wrapper wrapper--file">
+                <input class="file-input" id="js-file-input" name="myfile" type="file">
+                <div class="file-content">
+                <div class="file-infos">
+                    <p class="file-icon"><i class="fas fa-file-upload fa-2x"></i>
+                    <span class="icon-shadow"></span>
+                    <span>CLICK <span class="has-drag"> OR DRAG AND DROP</span></span>
+                    </p>
+                </div>
+                    <p class="file-name" id="js-file-name">Žádný soubor nebyl vybrán...</p>
+                </div>
+            </div>
+                
+            <div class="wrapper wrapper--number" id="counter">
+                <span>Počet studentů</span>
+                <div class="counter-minus btn btn-primary">-</div>
+                    <input type="text" value="1" name="pocetStudentu">
+                <div class="counter-plus btn btn-primary">+</div>
+            </div>
+                        
+            <div class="wrapper wrapper--number" id="counter2">
+                <span>Počet úloh</span>
+                <div class="counter-minus btn btn-primary">-</div>
+                    <input type="text" value="3" name="pocetUloh">
+                <div class="counter-plus btn btn-primary">+</div>
+            </div>
+                
+            <div class="wrapper wrapper--checkbox">
+                <span>Je součástí prohlášení?</span>
+                <input type="checkbox" id="isThereProhlaseni" name="isThereProhlaseni">
+                <label class="label label--prohlaseni" for="isThereProhlaseni">
+                    <div class="check"></div>
+                </label>
+            </div>
         
-    </form>
+            <div class="wrapper wrapper--checkbox">
+                <span>Chci tisknout prohlášení?</span>
+                <input type="checkbox" id="printProhlaseni" name="printProhlaseni">
+                <label class="label label--prohlaseni" for="printProhlaseni">
+                    <div class="check"></div>
+                </label>
+            </div>
+        
+            <div class="wrapper wrapper--checkbox">
+                <span>Chci tisknout i zadání úloh?</span>
+                <input type="checkbox" id="zadani" name="zadani" checked>
+                <label class="label label--zadani" for="zadani">
+                    <div class="check"></div>
+                </label>
+            </div>
 
+            <div class="wrapper wrapper--checkbox">
+                <span>Zahrnout všechny studenty</span>
+                <input type="checkbox" id="zahrnout-vse" name="zahrnout-vse" checked>
+                <label class="label label--zahrnout-vse" for="zahrnout-vse">
+                    <div class="check"></div>
+                </label>
+            </div>
+
+            <div id="students-name" class="wrapper wrapper--students-name">
+            <span>Jména studentů:</span>
+                <input type="text" name="jmenaStudentu" autocomplete="off" placeholder="Jmena studentu oddelena carkou">
+            </div>
+
+            <div class="wrapper wrapper--number" id="counter3">
+                <span>Počet souborů:</span>
+                <div id="counter3-minus" class="counter-minus btn btn-primary">-</div>
+                    <input type="text" value="0" name="pocetSouboru">
+                <div id="counter3-plus" class="counter-plus btn btn-primary">+</div>
+            </div>
+
+            <div id="generated-input" class="wrapper wrapper--generated-input">
+            </div>                
+            
+            <input type="submit" value="Stáhnout">
+            
+        </form>
+
+        <aside class="help help__right">
+
+        </aside>
+    </main>
+    
     <div class="file"></div>
-    </div>
 
     <!-- <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script> -->
     <script src="js/jquery-1.12.4.min.js"></script>
