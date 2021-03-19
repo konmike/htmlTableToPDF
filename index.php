d5ae9bb7b2082c8522ed2e92cb2ed00360ccbd0e
konmike <michalkonecny10@gmail.com>
2021-03-19 09:44:59 +0100

    Work on help component - grid system

--- index.php
+++ index.php
@@ -11,23 +11,18 @@
 </head>
 <body>
     <main role="main" class="main">
-        <aside class="help help__left">
-            <ul>
-                <li>
-                    Stáhněte si odevzdané práce studentů na webu moodle.prf.cuni.cz ve formátu HTML tabulka.
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" enable-background="new 0 0 100 100" xml:space="preserve"><path d="M88.6,79c-29.2-8.5-59.2-24.6-69-55.2c3.7,1.6,7.4,3.2,11.2,4.8c2.3,1,4.3-2.4,2-3.4c-6.1-2.6-12.2-5.2-18.4-7.9  c-2.2-1-4.2,2.2-2.2,3.3c-0.2,0.3-0.4,0.6-0.4,1.1c-0.3,4.8-0.7,9.6-1,14.3c-0.7,2.9-1,5.8-0.6,8.9c0.3,2.5,3.8,2.6,3.9,0  c0.2-2.7,0.4-5.5,0.6-8.2c0.1-0.5,0.2-1,0.4-1.5c0.2-0.6,0.1-1.2-0.2-1.6c0.2-3.2,0.4-6.4,0.7-9.6c9.9,32.4,41.2,49.7,72,58.7  C90,83.5,91,79.7,88.6,79z"/></svg>
-                </li>
-            </ul>
-        </aside>
-
         <form action="makepdf.php" method="post" class="form" enctype="multipart/form-data">
         
+            <div class="help help--upload">
+                <span>Stáhněte si odevzdané práce studentů na webu moodle.prf.cuni.cz ve formátu HTML tabulka.</span>
+            </div>
+
             <div class="upload">
                 <div class="file-infos">
                     <input class="input" id="js-file-input" name="myfile" type="file">
                     <p class="file-icon">
                         <i class="fas fa-file-upload fa-3x"></i>
-                        <label class="label">Zde klikni/<span class="has-drag">přetáhni soubor</span></label>
+                        <label class="label">Klikni/<span class="has-drag">přetáhni soubor</span></label>
                     </p>
                 </div>
                 <span class="file-name" id="js-file-name">Žádný soubor nebyl vybrán...</span>
@@ -39,7 +34,12 @@
                     <input class="input" readonly type="text" pattern="[0-9]+" value="0" name="pocetStudentu" >
                 <button class="btn add" disabled></button>
             </div>
-                        
+                     
+            <div class="help help--pocet-uloh">
+            <!-- <span>Stáhněte si odevzdané práce studentů na webu moodle.prf.cuni.cz ve formátu HTML tabulka.</span> -->
+                <span>Počet úloh k vypracování (bez čestného prohlášení).</span>
+            </div>
+
             <div class="number">
                 <label class="label">Počet úloh</label>
                 <button class="btn sub"></button>
@@ -113,9 +113,9 @@
             
         </form>
 
-        <aside class="help help__right">
+        <!-- <aside class="help help__right">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit provident nobis ratione nemo animi, illum fugit quo veniam ad quam exercitationem possimus, maxime asperiores sint aspernatur iusto libero? Delectus, eaque.
-        </aside>
+        </aside> -->
     </main>
     
     <div class="file"></div>
